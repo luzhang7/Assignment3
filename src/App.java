69fb192fb842e5ff7aab6e02bf24607be77cd042
Lu Zhang <zhang.lu7@northeastern.edu>
2023-02-08 16:47:37 -0800

    assignment3 commit

---
+++ src/App.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Information Systems Program @ Northeastern University College of Engineering
+ * Info 5001 | Application Modeling & Design
+ * 
+ * This is a sample project to make sure Visual Studio Code and all necessary components have been successfully installed.
+ * 
+ */
+
+public class App {
+
+    public static void main(String[] args) {
+        String welcomeMsg = "Lu Zhang";
+
+        System.out.println(welcomeMsg);
+    }
+}
